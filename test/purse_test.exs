1974b01cab8f77c10238cd64b494d2f824e04b7c
Portnyagin Maxim <mportnyagin@samokat.ru>
2025-07-08 14:25:42 +0200

    add spec

--- test/purse_test.exs
+++ test/purse_test.exs
@@ -1,8 +1,45 @@
 defmodule PurseTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Purse
 
-  test "greets the world" do
-    assert Purse.hello() == :world
+  setup do
+    {:ok, purse: Purse.create()}
+  end
+
+  test ".create", %{purse: purse} do
+    assert is_pid(purse)
+  end
+
+  test ".deposit", %{purse: purse} do
+    assert Purse.deposit(purse, "USD", 100) == %{"USD" => 100}
+  end
+
+  test ".withdraw", %{purse: purse} do
+    Purse.deposit(purse, "USD", 1000)
+
+    assert Purse.withdraw(purse, "USD", 100) == %{"USD" => 900}
+  end
+
+  test ".peek", %{purse: purse} do
+    Purse.deposit(purse, "USD", 1000)
+
+    assert Purse.peek(purse) == [{"USD", 1000}]
+    assert Purse.peek(purse, "USD") == 1000
+    assert Purse.peek(purse, "EUR") == 0
+  end
+
+  test ".transfer", %{purse: purse} do
+    purse2 = Purse.create()
+
+    Purse.deposit(purse, "USD", 1000)
+    Purse.deposit(purse, "EUR", 500)
+
+    assert Purse.peek(purse) == [{"EUR", 500}, {"USD", 1000}]
+    assert Purse.peek(purse2) == []
+
+    Purse.transfer(purse, purse2, "USD", 100)
+
+    assert Purse.peek(purse) == [{"EUR", 500}, {"USD", 900}]
+    assert Purse.peek(purse2) == [{"USD", 100}]
   end
 end
